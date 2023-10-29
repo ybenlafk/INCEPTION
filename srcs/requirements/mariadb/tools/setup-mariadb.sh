197d5aceea33ece26a9cd2d0d36cfca8cbf08823
Yahya Benlafkih <ybenlafk@e2r6p2.1337.ma>
2023-10-29 16:30:59 +0100

    :')

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# Start the MariaDB service
+# Start the MariaDB service (if not running already)
 service mysql start
 
-# Create the database and user
-mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+# Wait for MariaDB service to start (sleep might not be necessary in some cases)
+# Add a delay if required for service initialization
+sleep 10
 
-# Update the root password
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# Update the root password and grant privileges
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+# Create the database and user
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+# Stop the MariaDB service (you might not need to stop the service in a Docker environment)
+# service mysql stop
+
+# Start the MariaDB service in the foreground (let Docker manage the service)
+# exec mysqld_safe --user=mysql --console
--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -1,25 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-# Check if wp-config.php exists
-if [ -f ./wp-config.php ]; then
-    echo "WordPress already downloaded"
-else
-    # Download WordPress and its configuration files
-    echo "Downloading WordPress..."
-    wget -q https://wordpress.org/wordpress-5.8.tar.gz 
-    tar -xzvf wordpress-5.8.tar.gz
-    mv wordpress/* .
-    rm -rf wordpress-5.8.tar.gz wordpress
+cd /var/www/html
 
-    # Import environment variables in the config file
-    sed -i "s/username_here/ybenlafk/g" wp-config-sample.php
-    sed -i "s/password_here/1234/g" wp-config-sample.php
-    sed -i "s/localhost/mariadb/g" wp-config-sample.php
-    sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
+# Download and install WP-CLI
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-    cp wp-config-sample.php wp-config.php
+# Download WordPress Core
+wp core download --allow-root
 
-    echo "WordPress installation and configuration completed."
-fi
+# Move the sample configuration file to wp-config.php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# Set up the database configuration in wp-config.php
+sed -i -r "s/database_name_here/$MYSQL_DATABASE/1" wp-config.php
+sed -i -r "s/username_here/$MYSQL_USER/1" wp-config.php
+sed -i -r "s/password_here/$MYSQL_PASSWORD/1" wp-config.php
 
-exec "$@"
+sleep 20
+# Install WordPress
+wp core install --url=$DOMAIN_NAME/ --title=$TITLE --admin_user=$ADMINE_USER --admin_password=$ADMINE_PW --skip-email --allow-root
+
+# Create an additional user
+wp user create $USER --role=author --user_pass=$PWD --allow-root
+
+# Configure PHP-FPM
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+mkdir /run/php
+
+# Start PHP-FPM service
+/usr/sbin/php-fpm7.3 -F
