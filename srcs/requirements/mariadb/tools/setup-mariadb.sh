10045c9a3f410d1b7866c6edf1b6a7dd60b4c3ef
Yahya Benlafkih <ybenlafk@e1r6p14.1337.ma>
2023-10-30 14:40:11 +0100

    :')

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -5,21 +5,22 @@ service mysql start
 
 # Wait for MariaDB service to start (sleep might not be necessary in some cases)
 # Add a delay if required for service initialization
-sleep 10
+sleep 5
 
 # Update the root password and grant privileges
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # Create the database and user
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
+# echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # Stop the MariaDB service (you might not need to stop the service in a Docker environment)
-# service mysql stop
+service mysql stop
 
 # Start the MariaDB service in the foreground (let Docker manage the service)
-# exec mysqld_safe --user=mysql --console
+exec mysqld_safe --user=mysql --console
+
+# exec "tail -f /dev/null"
