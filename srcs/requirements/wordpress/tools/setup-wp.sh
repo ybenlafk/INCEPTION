1e5a62f654687710ceb12ce3b5bdaa512abd0889
Yahya Benlafkih <ybenlafk@e2r5p9.1337.ma>
2023-10-31 20:40:27 +0100

    :))

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -10,7 +10,7 @@ else
     tar -xzvf wordpress-5.8.tar.gz
     mv wordpress/* .
     rm -rf wordpress-5.8.tar.gz wordpress
-
+    
     # Import environment variables in the config file
     sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
     sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
@@ -20,6 +20,25 @@ else
     cp wp-config-sample.php wp-config.php
 
     echo "WordPress installation and configuration completed."
+
+    # Add two users (including one admin)
+    if [ -f ./wp-config.php ]; then
+        echo "WordPress already installed. Adding users..."
+
+        curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
+        chmod +x wp-cli.phar 
+
+        mv wp-cli.phar /usr/local/bin/wp
+
+        wp core install --url="localhost" --title=$TITLE --admin_user="$ADMINE_USER" --admin_password="$ADMINE_PWD" --admin_email="$ADMINE_EMAIL" --allow-root
+        
+        wp user create "$USER" "$EMAIL" --user_pass="$PWD" --allow-root
+        
+        echo "Two users have been created (one admin)."
+    else
+        echo "WordPress not found. Installation failed."
+    fi
 fi
 
 exec "$@"
