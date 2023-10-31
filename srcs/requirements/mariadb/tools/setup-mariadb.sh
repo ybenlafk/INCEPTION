2e5f296517cf8ea7d718d13ed48d47e3899b5bbc
Yahya Benlafkih <ybenlafk@e2r5p9.1337.ma>
2023-10-31 13:48:12 +0100

    :')

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -7,20 +7,19 @@ service mysql start
 # Add a delay if required for service initialization
 sleep 5
 
-# Update the root password and grant privileges
-
 # Create the database and user
+echo "UPDATE mysql.user SET Host='%' WHERE User='root';" | mysql -u root
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-# echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root
+# Update the root password and grant privileges
+echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
+echo "INSERT INTO mysql.user (Host, User, ssl_cipher, x509_issuer,x509_subject,authentication_string) VALUES ('127.0.0.1', 'root', '', '', '', '');" | mysql -u root
+echo "INSERT INTO mysql.user (Host, User, ssl_cipher, x509_issuer,x509_subject,authentication_string) VALUES ('::1', 'root', '', '', '', '');" | mysql -u root
 
 # Stop the MariaDB service (you might not need to stop the service in a Docker environment)
 service mysql stop
 
 # Start the MariaDB service in the foreground (let Docker manage the service)
 exec mysqld_safe --user=mysql --console
-
-# exec "tail -f /dev/null"
