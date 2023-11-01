5e0be108ce65a9505fc88e0b75daa2f864216fb8
Yahya Benlafkih <ybenlafk@e2r6p7.1337.ma>
2023-11-01 14:59:37 +0100

    ;)

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -4,12 +4,16 @@
 if [ -f ./wp-config.php ]; then
     echo "WordPress already downloaded"
 else
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+    chmod +x wp-cli.phar 
+    mv wp-cli.phar /usr/local/bin/wp
+
     # Download WordPress and its configuration files
     echo "Downloading WordPress..."
-    wget -q https://wordpress.org/wordpress-5.8.tar.gz 
-    tar -xzvf wordpress-5.8.tar.gz
-    mv wordpress/* .
-    rm -rf wordpress-5.8.tar.gz wordpress
+
+    wp core download --allow-root
+
+    echo "WordPress downloaded."
     
     # Import environment variables in the config file
     sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
@@ -25,11 +29,6 @@ else
     if [ -f ./wp-config.php ]; then
         echo "WordPress already installed. Adding users..."
 
-        curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-        chmod +x wp-cli.phar 
-
-        mv wp-cli.phar /usr/local/bin/wp
 
         wp core install --url="localhost" --title=$TITLE --admin_user="$ADMINE_USER" --admin_password="$ADMINE_PWD" --admin_email="$ADMINE_EMAIL" --allow-root
         
